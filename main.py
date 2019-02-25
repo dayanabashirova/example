f27f1a4fb69e0dfafce6f0883da3a79b145fa28e
tansunasyrova <47747253+tansunasyrova@users.noreply.github.com>
2019-02-25 16:55:19 +0300

    canges (#1)
    * commit pycharm all

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 
 def hello(name):
+    """
+    YOUR DOCSTRINGS......
+    :param name:
+    :return:
+    """
     return 'hello ' + name
+
+
 def good_bye2(name):
+    """
+    YOUR DOCSTRINGS......
+    :param name:
+    :return:
+    """
     return 'gb' + name 
